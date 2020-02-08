7f4c81a6caf53556d03b753d46374fcca872052c
Your Name <social@coderwelsch.com>
2020-02-08 18:19:28 +0100

    It displays the current 2 trams now

--- esp32/VBB_Fetch/VBB_Fetch.ino
+++ esp32/VBB_Fetch/VBB_Fetch.ino
@@ -1,66 +1,158 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// config
 #include "config.h";
 
+// globals
 const char* url = "https://projects.coderwelsch.com/misc/vbb-display/";
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+struct Tram_Struct {
+	char name[8];
+	int timeLeft;
+	int initialTime;
+	char line[2];
+};
+
+struct Tram_Struct tram1;
+struct Tram_Struct tram2;
+
+bool dataFetched = false;
+bool dataDisplayed = false;
+
+int lastTickMillis = 0;
+int TRAM_DEPARTURE_DELAY = 180;
 
 
 void setup() {
-	Serial.begin(115200);
-	delay(4000);
-	WiFi.begin(WIFI_SSID, WIFI_PWD);
- 
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(1000);
-		Serial.println("Connecting to WiFi..");
-	}
- 
-	Serial.println("Connected to the WiFi network");
+	// lcd
+	lcd.begin(16, 2);
+
+	startWiFi();
+}
+
+void updateLastMillis () {
+	lastTickMillis = millis();
 }
  
 void loop () {
-  JsonArray jsonArray = getJsonData;
-  
-	if ((WiFi.status() == WL_CONNECTED)) { // Check the current connection status
-		HTTPClient http;
- 
-		http.begin(url); // Specify the URL
-		int httpCode = http.GET();
- 
-		if (httpCode > 0) { // Check for the returning code
-				String payload = http.getString();
-				Serial.println(httpCode);
-				Serial.println(payload);
-				
-				// https://arduinojson.org/v6/assistant/
-				const size_t jsonDataSize = JSON_ARRAY_SIZE(15) + 15 * JSON_OBJECT_SIZE(3);
-				StaticJsonDocument<jsonDataSize> doc;
-				DeserializationError error = deserializeJson(doc, payload);
+	if (
+		dataFetched && 
+		(tram1.timeLeft > TRAM_DEPARTURE_DELAY) &&
+		(tram2.timeLeft > TRAM_DEPARTURE_DELAY)
+	) {
+		displayTimes();
+	} else if ((WiFi.status() == WL_CONNECTED)) {
+		Serial.println("FETCH");
 
-				if (error) {
-					Serial.println("Datenfehler: " + String(error.c_str()));
-				} else {
-					jsonArray = doc.as<JsonArray>();
+		getData();
+		displayTimes();
 
-					for (JsonVariant tram : jsonArray) {
-						const char* direction = tram["d"];
-						const char* tramLine = tram["n"];
-						int time = tram["t"].as<int>();
+		Serial.println("FETCHED");
 
-						Serial.printf("%s %s | %s min\n", tramLine, direction, String(time / 60));
-					}
-				}
-		} else {
-			Serial.println("Error on HTTP request");
-		}
- 
-		http.end(); //Free the resources
+		dataFetched = true;
+		dataDisplayed = true;
+	} else {
+		lcd.clear();
+		lcd.println("WiFi not connected");
 	}
- 
-	delay(10000);
 }
 
-JsonArray getJsonData () {
-  
+void displayTimes () {
+	// check just every ten seconds if the display
+	// should update something
+	const int secondsPassed = (millis() - lastTickMillis) / 1000;
+
+	if (secondsPassed >= 10) {
+		// update time threshold
+		updateLastMillis();
+
+		Serial.println("UPDATE LCD TIME");
+
+		displayTramLine(&tram1, 0, secondsPassed);
+		displayTramLine(&tram2, 1, secondsPassed);
+	}
+}
+
+void displayTramLine (Tram_Struct *tram, byte line, int secondsPassed) {
+	// check if tram display has to update
+	int minsBefore = tram->timeLeft / 60;
+	int minsAfter = (tram->timeLeft - secondsPassed) / 60;
+
+	int minsLeft = tram->timeLeft / 60;
+	String time = minsLeft > 9 ? String(minsLeft) : " " + String(minsLeft);
+
+	tram->timeLeft -= secondsPassed;
+
+	lcd.setCursor(0, line);
+	lcd.print(
+		String(tram->line) + " " +
+		String(strndup(tram->name, 3)) + " | " +
+		time + " min"
+	);
+}
+
+void startWiFi () {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+
+	Serial.begin(115200);
+	delay(4000);
+	WiFi.begin(WIFI_SSID, WIFI_PWD);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(1000);
+		lcd.print("Connect WiFi ...");
+	}
+
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("WiFi Connected");
+}
+
+void getData () {
+	HTTPClient http;	
+ 
+	http.begin(url); // Specify the URL
+	int httpCode = http.GET();
+
+	if (httpCode > 0) { // Check for the returning code
+			String payload = http.getString();
+
+			// https://arduinojson.org/v6/assistant/
+			const size_t jsonDataSize = JSON_ARRAY_SIZE(4) + 4 * JSON_OBJECT_SIZE(3);
+			StaticJsonDocument<jsonDataSize> doc;
+			
+			DeserializationError error = deserializeJson(doc, payload);
+
+			if (error) {
+				lcd.clear();
+				lcd.setCursor(0, 0);
+				lcd.println("Datenfehler:");
+				lcd.setCursor(0, 1);
+				lcd.println(String(error.c_str()));
+			} else if (doc.size()) {
+				strcpy(tram1.name, doc[0]["d"].as<char*>());
+				tram1.initialTime = tram1.timeLeft = doc[0]["t"].as<int>();
+				strcpy(tram1.line, doc[0]["n"].as<char*>());
+
+				strcpy(tram2.name, doc[1]["d"].as<char*>());
+				tram2.initialTime = tram2.timeLeft = doc[1]["t"].as<int>();
+				strcpy(tram2.line, doc[1]["n"].as<char*>());
+			}
+	} else {
+		lcd.clear();
+		lcd.setCursor(0, 0);
+		lcd.print("HTTP Fehler:");
+
+		lcd.setCursor(0, 1);
+		lcd.print(String(httpCode));
+	}
+
+	http.end();
 }
