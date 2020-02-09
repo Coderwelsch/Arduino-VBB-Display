f15182c0584f128f821598b3a4ab4a441746d1c9
Your Name <social@coderwelsch.com>
2020-02-09 13:05:54 +0100

    Removed config.h and replaced it with a config_example.h

--- esp32/VBB_Fetch/config.h
+++ esp32/VBB_Fetch/config.h
@@ -1,2 +1,9 @@
-# define WIFI_SSID = "";
-# define WIFI_PWD = "";
+// WIFI login data
+// please mind: esp32 only supports 2.4 Ghz
+#define WIFI_SSID "Club-Der-Dichten-Toten-2-4GHz"
+#define WIFI_PWD "366FF8XW967868JW"
+
+// tram rest api config
+#define FROM_TRAM_STATION "736150"
+#define TO_TRAM_STATION "736100"
+#define LENGTH_OF_STATION_DATA 5
---
+++ esp32/VBB_Fetch/config_example.h
@@ -0,0 +1,9 @@
+// WIFI login data
+// please mind: esp32 only supports 2.4 Ghz
+#define WIFI_SSID ""
+#define WIFI_PWD ""
+
+// tram rest api config
+#define FROM_TRAM_STATION "" // string
+#define TO_TRAM_STATION "" // string
+#define LENGTH_OF_STATION_DATA 6 // byte
